514bed2792f14440936b542f6e26fd1dfb3d8fa5
chua336699-dev <chua336699@gmail.com>
2026-09-22 13:48:54 +0800

    Create config.js

---
+++ config.js
@@ -0,0 +1,6 @@
+// Copy this file to config.js. config.js is ignored by Git.
+// Use the project root URL, not the /rest/v1 endpoint.
+window.QUESTLOG_SUPABASE = {
+  url: "https://djewbjqdiovkdxrxpypy.supabase.co",
+  anonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRqZXdianFkaW92a2R4cnhweXB5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3OTAwNDEwNTIsImV4cCI6MjEwNTYxNzA1Mn0.eQQVuZg_QqV4Y9XpaFUebfRnHRMKXnz-R8JLeZP0BDg"
+};
